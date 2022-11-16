eb4527f2bed17371572eaca8b8052dacbfeaa439
tatsukamijo <tatsukamijo@icloud.com>
2022-11-17 00:11:52 +0900

    added waypoints

--- Assets/Scripts/WayPoint.cs
+++ Assets/Scripts/WayPoint.cs
@@ -25,13 +25,15 @@ public class WayPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         // 目的地点までの距離(remainingDistance)が目的地の手前までの距離(stoppingDistance)以下になったら
         if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
         {
             // 目的地の番号を１更新（右辺を剰余演算子にすることで目的地をループさせれる）
-            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            currentWaypointIndex += 1;
             // 目的地を次の場所に設定
             navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
         }
+
     }
 }
