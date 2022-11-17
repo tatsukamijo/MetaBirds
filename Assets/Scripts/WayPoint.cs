973c21806e3ce4e50bbf7cffd34df5d4ead196af
tatsukamijo <tatsukamijo@icloud.com>
2022-11-17 10:20:50 +0900

    fixes

--- Assets/Scripts/WayPoint.cs
+++ Assets/Scripts/WayPoint.cs
@@ -16,7 +16,7 @@ public class WayPoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // navMeshAgent変数にNavMeshAgentコンポーネントを入れる
+       // navMeshAgent変数にNavMeshAgentコンポーネントを入れる
         navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         // 最初の目的地を入れる
         navMeshAgent.SetDestination(waypoints[0].position);
@@ -25,16 +25,13 @@ public class WayPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         // 目的地点までの距離(remainingDistance)が目的地の手前までの距離(stoppingDistance)以下になったら
         if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
         {
-            Debug.Log($"waypoint{currentWaypointIndex}");
-            // 目的地の番号を１更新（右辺を剰余演算子にすることで目的地をループさせれる）
+            Debug.Log($"waypoint{currentWaypointIndex} haspath{navMeshAgent.hasPath}");
             currentWaypointIndex += 1;
             // 目的地を次の場所に設定
             navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
         }
-
     }
 }
