9515844c81515c65b23eab8384efcea52b9ba5b1
tatsukamijo <tatsukamijo@icloud.com>
2022-11-19 15:05:12 +0900

    animation

--- Assets/Scripts/CalcForce.cs
+++ Assets/Scripts/CalcForce.cs
@@ -14,11 +14,17 @@ public class CalcForce : MonoBehaviour
     void Update()
     {
         Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
+        float speed = 1000.0f;
+        /*
            // 入力をxとzに代入
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         //Rigidbodyに力を加える
         rb.AddForce(3000 * x, 6000 * z, 6000 * z);
+        */
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+        rb.AddForce(x, 0.5f*z, 100f*z);
     }
 
     void FixedUpdate()
