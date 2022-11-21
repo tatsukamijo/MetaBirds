3fcf9dc719a9861f732f31ef5c179a9cea66c6f6
tatsukamijo <tatsukamijo@icloud.com>
2022-11-22 00:32:00 +0900

    few fixes

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -7,30 +7,21 @@ public class AgentController : MonoBehaviour
 {
     public Transform goal;
     public Transform tori;
-    public Vector3 nextPosition;
-
-    NavMeshPath path;
     private UnityEngine.AI.NavMeshAgent agent;
-    const int maxPosition = 9;
-    Vector3[] positions = new Vector3[maxPosition];
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        path = new NavMeshPath();
-        agent.CalculatePath(goal.position, path);
-        path.GetCornersNonAlloc(positions);
     }
 
     // Update is called once per frame
     void Update()
     {
         CollisionManager collisionmanager;
-        GameObject col_obj = GameObject.Find("fly");
-        collisionmanager = col_obj.GetComponent<CollisionManager>();
+        collisionmanager = GameObject.Find("fly").GetComponent<CollisionManager>();
 
-        // ここをis_Mutekiで場合分け.どっちからどっちの座標を一致させるか.
+        //  is_Mutekiで制御主体を切り替え
         if (!collisionmanager.is_Muteki)
         {
             this.transform.position = tori.position;
@@ -40,16 +31,6 @@ public class AgentController : MonoBehaviour
             agent.speed = 300;
             agent.acceleration = 300;
             agent.SetDestination(goal.position);
-        }
-
-        nextPosition = agent.path.corners[0];
-        // Debug.Log($"{agent.nextPosition}");
-    }
-    private void onDrawGizmoSelected()
-    {
-        for (int i = 0; i < positions.Length - 1; i++)
-        {
-            Gizmos.DrawLine(positions[i], positions[i + 1]);
         }
     }
 }
--- Assets/Scripts/CalcForce.cs
+++ Assets/Scripts/CalcForce.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 
 public class CalcForce : MonoBehaviour
 {
-    /*
-    private CharacterController characterController;
-    private Vector3 velocity;
-    */
+ 
     private Rigidbody rb;
-    private Vector3 pos_dif;
     public Transform agent;
 
     // Start is called before the first frame update
@@ -17,53 +13,28 @@ public class CalcForce : MonoBehaviour
     {
         Debug.Log("start");
         rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
-
-        /*
-        characterController = GetComponent<CharacterController>();
-        transform.position = new Vector3(0f, 1f, 0f);
-        */
     }
 
     // Update is called once per frame
     void Update()
     {
         AgentController agentcontroller;
-        GameObject agent_obj = GameObject.Find("tori_agent");
-        agentcontroller = agent_obj.GetComponent<AgentController>();
+        agentcontroller = GameObject.Find("tori_agent").GetComponent<AgentController>();
 
         CollisionManager collisionmanager;
-        GameObject col_obj = GameObject.Find("fly");
-        collisionmanager = col_obj.GetComponent<CollisionManager>();
+        collisionmanager = GameObject.Find("fly").GetComponent<CollisionManager>();
+
         if (!collisionmanager.is_Muteki)
         {
-        float speed = 1000.0f;
-        float x = Input.GetAxis("Horizontal") * speed;
-        float z = Input.GetAxis("Vertical") * speed;
-        rb.AddForce(50f * x, 50f * z, 50f * z);
+            float speed = 1000.0f;
+            float x = Input.GetAxis("Horizontal") * speed;
+            float z = Input.GetAxis("Vertical") * speed;
+            rb.AddForce(50f * x, 50f * z, 50f * z);
         }
-        else // is_Mutekiのとき. agentの位置にtoriを合わせる.
+        else // is_Mutekiのとき. agentが制御.
         {
             this.transform.position = agent.position;
-            /*
-            // 座標系の変換が必要そう.
-        float speed = 10.0f;
-        float forward_x = transform.forward.x * speed;
-        float forward_y = transform.forward.y * speed;
-        float forward_z = transform.forward.z * speed;
-            rb.velocity = new Vector3(0f, 0f, 0f);
-        // rb.velocity = new Vector3(forward_x, forward_y, forward_z);
-        */
-        Debug.Log($"{collisionmanager.is_Muteki}");
+            Debug.Log($"{collisionmanager.is_Muteki}");
         }
-
-        /*
-        velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        characterController.Move(velocity);
-        */
-    }
-
-    void FixedUpdate()
-    {
-
     }
 }
