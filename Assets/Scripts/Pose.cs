15b3d8931744a6da26652f215151d231a90b0e28
tatsukamijo <tatsukamijo@icloud.com>
2022-11-26 00:38:55 +0900

    serial pose

--- Assets/Scripts/Pose.cs
+++ Assets/Scripts/Pose.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class Pose : MonoBehaviour
 {
+    public SerialHandler serialHandler;
+    public string[] data;
+    public float acceleration;
+    public float tilt;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        acceleration = 0f;
+        tilt = 0f;
+        serialHandler.OnDataReceived += OnDataReceived;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 localAngle = this.transform.localEulerAngles;
+        localAngle.x = tilt;
+        localAngle.y = 97.011f;
+        localAngle.z = -20.215f;
+        this.transform.localEulerAngles = localAngle;
+        /*
         // →キー押下時
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
@@ -30,5 +42,24 @@ public class Pose : MonoBehaviour
             localAngle.z = -20.215f;
             this.transform.localEulerAngles = localAngle;
         }
+        */
     }
+
+    void OnDataReceived(string message)
+    {
+        data = message.Split(
+            new string[] { "\n" }, System.StringSplitOptions.None);
+        if (data.Length != 1) return;
+        var hoge = float.Parse(data[0]);
+        Debug.Log(hoge);
+        if (hoge > 5000f)
+        {
+            tilt = hoge - 10000f;
+        }
+        else
+        {
+            acceleration = hoge;
+        }
+    }
+
 }
