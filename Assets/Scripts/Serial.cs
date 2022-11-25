79ac21790df283c608ca0d5b88a9da1b6e9113a1
tatsukamijo <tatsukamijo@icloud.com>
2022-11-25 13:37:22 +0900

    added script

---
+++ Assets/Scripts/Serial.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ConsoleOutput : MonoBehaviour
+{
+    public SerialHandler serialHandler;
+
+    void Start()
+    {
+        serialHandler.OnDataReceived += OnDataReceived;
+    }
+
+    void OnDataReceived(string message)
+    {
+        var data = message.Split(
+            new string[] { "\n" }, System.StringSplitOptions.None);
+        if (data.Length != 1) return;
+        Debug.Log(data[0]);
+    }
+}
