833dd601e966cd7d4fde8cc0ef546e18c1c24491
shogo-kitamura <kitamura-shogo523@g.ecc.u-tokyo.ac.jp>
2022-12-01 13:49:54 +0900

    itemmaker update

--- Assets/Scripts/ItemMaker.cs
+++ Assets/Scripts/ItemMaker.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class ItemMaker : MonoBehaviour
 {
-    public GameObject prefabObj;
+    public GameObject prefabObj1;
+    public GameObject prefabObj2;
     public GameObject start;
     public GameObject end;
     public int N;
     public float rand_x;
     public float rand_y;
     public float rand_z;
+    public float mix_rate;
     
     // Start is called before the first frame update
     void Start()
     {
         Vector3 startPos = start.transform.position;
         Vector3 endPos = end.transform.position;
-        Vector3 dr = (endPos - startPos) / (N - 1);
-        for (int i = 0; i < N; i++)
+        Vector3 dr = (endPos - startPos) / (N + 1);
+        for (int i = 1; i <= N; i++)
         {
             Vector3 randomness = new Vector3(Random.Range(-rand_x, rand_x), Random.Range(-rand_y, rand_y), Random.Range(-rand_z, rand_z));
-            GameObject obj = Instantiate(prefabObj, startPos + dr * i + randomness, Quaternion.identity);
+            if (Random.Range(0.0f, 1.0f) > mix_rate)
+            {
+                GameObject obj = Instantiate(prefabObj1, startPos + dr * i + randomness, Quaternion.identity);
+            }
+            else
+            {
+                GameObject obj = Instantiate(prefabObj2, startPos + dr * i + randomness, Quaternion.identity);
+            }
         }
     }
 }
