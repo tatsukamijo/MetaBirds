82a939ac37f1cea9ce7ee7f205ac4fef51e94c16
shogo-kitamura <kitamura-shogo523@g.ecc.u-tokyo.ac.jp>
2022-12-01 14:41:45 +0900

    building setter

---
+++ Assets/Scripts/buildingSetter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildingSetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] collection;
+    public int N;
+    public float startX;
+    public float startZ;
+    public float offset;
+    void Start()
+    {
+        float x = startX;
+        float z = startZ;
+        for (int i = 0; i < N; i++)
+        {
+            int n = Random.Range(0, collection.Length);
+            Instantiate(collection[n], new Vector3(x, 0, z), Quaternion.identity);
+            z += 100 + offset;
+        }
+    }
+}
