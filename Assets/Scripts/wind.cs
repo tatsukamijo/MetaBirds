e39ba4afcd1a032936f30f1608e9ef34ed4bca9e
shogo-kitamura <kitamura-shogo523@g.ecc.u-tokyo.ac.jp>
2022-12-02 14:43:14 +0900

    add audio

--- Assets/Scripts/wind.cs
+++ Assets/Scripts/wind.cs
@@ -7,11 +7,13 @@ public class wind : MonoBehaviour
     public GameObject target;
     public Vector3 force;
     Rigidbody rb;
+    AudioSource audioSource; 
     
     // Start is called before the first frame update
     void Start()
     {
         rb = target.GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,12 +22,30 @@ public class wind : MonoBehaviour
         
     }
 
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.attachedRigidbody == rb)
+        {
+            audioSource.Play();
+            Debug.Log("play");
+        }
+    }
+
     void OnTriggerStay(Collider col)
     {
         if (col.attachedRigidbody == rb)
         {
             rb.AddForce(force);
-            Debug.Log("hit");
+            //Debug.Log("hit");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.attachedRigidbody == rb)
+        {
+            audioSource.mute = true;
+            Debug.Log("end");
         }
     }
 }
