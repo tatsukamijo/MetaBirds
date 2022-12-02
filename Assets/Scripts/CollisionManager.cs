8cc1b61fa725f94bba7bfc9eb752be016c7798d8
shogo-kitamura <kitamura-shogo523@g.ecc.u-tokyo.ac.jp>
2022-12-02 13:39:52 +0900

    time

--- Assets/Scripts/CollisionManager.cs
+++ Assets/Scripts/CollisionManager.cs
@@ -13,6 +13,14 @@ public class CollisionManager : MonoBehaviour
     public bool is_GameOver;
     public bool is_Goal;
     public float time;
+    public AudioClip BGM_main;
+    public AudioClip BGM_muteki;
+    public AudioClip BGM_gameover;
+    public AudioClip BGM_goal;
+    public AudioClip SE_item;
+    public AudioClip SE_muteki;
+    public AudioClip SE_damage;
+    AudioSource audioSource;
     private UnityEngine.AI.NavMeshAgent agent;
 
     // Start is called before the first frame update
@@ -22,6 +30,9 @@ public class CollisionManager : MonoBehaviour
         is_GameOver = false;
         is_Goal = false;
         time = 0.0f;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = BGM_main;
+        audioSource.Play();
     }
 
     // Update is called once per frame
@@ -40,6 +51,7 @@ public class CollisionManager : MonoBehaviour
         {
             Destroy(col.gameObject);
             Debug.Log("Item");
+            audioSource.PlayOneShot(SE_item);
         }
         else if (col.gameObject.tag == "Muteki")
         {
@@ -47,23 +59,31 @@ public class CollisionManager : MonoBehaviour
             Destroy(col.gameObject);
             Debug.Log("Muteki");
             Debug.Log("heading to the goal");
+            audioSource.PlayOneShot(SE_muteki);
+            audioSource.clip = BGM_muteki;
+            audioSource.Play();
         }
         else if (col.gameObject.tag == "Goal")
         {
             is_Muteki = false;
             Destroy(col.gameObject);
             Debug.Log("reached goal");
+            audioSource.clip = BGM_main;
+            audioSource.Play();
         }
         else if (col.gameObject.tag == "Wall")
         {
 
         }
         else if (col.gameObject.tag != "Item" && col.gameObject.tag != "Muteki")
-        {
+        { 
             Debug.Log("{col.gameObject.tag}");
             gameOverText.SetActive(true);
             is_GameOver = true;
+            audioSource.PlayOneShot(SE_damage);
             Debug.Log("Collision");
+            audioSource.clip = BGM_gameover;
+            audioSource.Play();
         }
     }
 }
